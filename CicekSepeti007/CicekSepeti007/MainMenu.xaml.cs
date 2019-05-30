64b5f6d0d8f99229f6d8c8677511d7c98349de7b
Şeyma <lax_files@outlook.com>
2019-05-30 07:34:49 +0300

    Add project files.

---
+++ CicekSepeti007/CicekSepeti007/MainMenu.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CicekSepeti007
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainMenu : ContentPage
+	{
+		public MainMenu ()
+		{
+			InitializeComponent();
+		}
+
+        private void Click_UyeGirisi(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainMenuUyeGirisi(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Click_DogumGunu(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
